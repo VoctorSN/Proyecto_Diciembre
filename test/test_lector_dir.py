6df928a980ea382dd358dd437f4424a58c94f347
Victor Clase <victorsancheznogueira@gmail.com>
2023-12-01 13:17:34 +0100

    Arreglado el problema de los logs

--- test/test_lector_dir.py
+++ test/test_lector_dir.py
@@ -2,8 +2,4 @@ from src.lector_dir import lector_dir
 
 
 def test_archivos():
-    assert "Mejor cancion.mp3" in lector_dir()
-
-def test_solo_mp3():
-    assert "Mejor cancion.mp3" in lector_dir()
-    assert "pruebamala" not in lector_dir()
+    assert "file:///D:/P/python/Proyecto_diciembre/songs/A_Virtual_Friend_-_Clair_Obscur.mp3" in lector_dir()
