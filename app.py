354cc793fabb36f856a5113b3e47bc11b236ddae
Victor Clase <victorsancheznogueira@gmail.com>
2023-12-01 13:46:50 +0100

    vlc_conector creado y app en funcionamiento

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from src.randomizer import randomizer
 from src.lector_dir import lector_dir
+from src.vlc_conector import vlc_conector
 
 def app():
-    return randomizer(lector_dir())
+    vlc_conector(randomizer(lector_dir()))
+    
 if __name__ == "__main__":
-    print(app())
+    app()
---
+++ src/vlc_conector.py
@@ -0,0 +1,9 @@
+import subprocess
+
+def vlc_conector(urls_random):
+    comando=["C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"] + urls_random
+    subprocess.run(comando, check=False)
+
+
+if __name__ == "__main__":
+    vlc_conector(['file:///D:/P/python/Proyecto_diciembre/songs/A_Virtual_Friend_-_Clair_Obscur.mp3', 'file:///D:/P/python/Proyecto_diciembre/songs/Boukmanflow_-_Does_she_not.mp3', 'file:///D:/P/python/Proyecto_diciembre/songs/Mejor%20cancion.mp3', 'file:///D:/P/python/Proyecto_diciembre/songs/PUETO_PA_LO_MIO_RECORDS_-_YO_NO_ENTIENDO_-_PUETO_PA_LO_MIO_RECORDS_Presenta_-_-_NEYCOM.mp3'])
