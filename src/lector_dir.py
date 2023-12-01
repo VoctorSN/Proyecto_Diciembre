aab9c208e76fb864b406ee5d49966b8ca1bcf308
Victor Clase <victorsancheznogueira@gmail.com>
2023-12-01 13:30:33 +0100

    prueba merge

--- src/lector_dir.py
+++ src/lector_dir.py
@@ -3,6 +3,8 @@ def lector_dir():
     return [element.text for element in 
             list(ET.parse('canciones.xspf').getroot().iter("{http://xspf.org/ns/0/}location"))]
 
+
+
 if __name__ == "__main__":
     print(lector_dir())
     # Si esta bien deberia dar las localizaciones de las canciones del fichero xspf
